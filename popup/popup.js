d6eb00d13f8485b7822ee120f38c4c839908a97e
Muhammad Kamran <mkamran1967@gmail.com>
2020-10-29 21:46:44 -0700

    UI functionality test, added add/delete/reload functionality.

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', onLoaded, false);
 
 function onLoaded() {
   // Check for previous list of items and load them
+  reloadList();
 
   // Save by "Enter" key
   document.getElementById('addWord').addEventListener('keypress', (e) => {
@@ -15,14 +16,17 @@ function onLoaded() {
   // Add via Submit click || Delete via click.
   document.getElementById('container').addEventListener('click', (e) => {
     let parentEl = e.target.parentElement.parentElement;
+
     // Check element type
     // For Enter/Submit click
     if (parentEl.className === 'submit') {
+      const incomingWord = document.getElementById('addWord').value;
       // Add Word to list
-      saveToLocal(document.getElementById('addWord').value);
+      saveToLocal(incomingWord);
     } else if (parentEl.tagName === 'LI') {
       console.log(`Close LI Button pressed`);
-      // TODO: Delete word
+      //
+      deleteWord(parentEl.firstElementChild.innerText);
     }
   });
 }
@@ -36,69 +40,39 @@ function saveToLocal(word) {
    * 4. Call reload function
    */
 
-  let wordArray = []; // Will hold words.
+  // Check if incoming word is valid.
+  if (word !== '') {
+    word = word.trim(); // Trim spaces from both ends of the word.
+    // Get previous list of word(s)
+    chrome.storage.local.get('wordList', ({ wordList }) => {
+      let wordArray = []; // Will hold words.
 
-  chrome.storage.local.get('wordList', (res) => {
-    console.log(res.wordList);
+      // Check if wordList exists
+      if (typeof wordList !== 'undefined' && wordList.length > 0) {
+        console.log(`WordList is defined`);
+        // Check if word exists in wordList, if doesn't exist ->
+        if (wordList.indexOf(word) === -1) {
+          console.log(`Word was not found in the list. ${word}`);
+          wordArray = [...wordList, word];
+        } // if
+        else {
+          // Word was found and the function is returned; -> ignored
+          return;
+        }
+      } // if
+      else {
+        console.log(`List DNE & ${word} was entered`);
+        // This runs when the wordList is DNE
+        wordArray.push(word);
+      }
 
-    // Check if list exists. (This case wordArray[0] => False)
-    if (typeof res.wordList === 'undefined') {
-      // DNE
-      wordArray.push(word);
-    } else {
-      wordArray = [...res.wordList, word];
-    }
-
-    // Save list to localstorage
-    chrome.storage.local.set({ wordList: wordArray }, function () {
-      // On save reload list.
-      console.log(`Saved ${word}`);
-
-      // Call reloadList function
-      reloadList();
-    });
-  });
-}
-
-// function saveToLocal(word) {
-//   /**
-//    * This function will do 4 things
-//    * 1. Get previous data
-//    * 2. Check if incoming word exists if yes -> ignore
-//    * 3. Else add current word to array and push array to storage
-//    * 4. Call reload function
-//    */
-
-//   let wordArray = []; // Will hold words.
-
-//   wordArray = getFromLocal();
-
-//   // Check if list exists. (This case wordArray[0] => False)
-//   if (!wordArray[0]) {
-//     wordArray[0] = word;
-//   } else {
-//     wordArray.push(word);
-//   }
-
-//   // Save list to localstorage
-//   chrome.storage.local.set({ wordList: wordArray }, function () {
-//     // On save reload list.
-//     console.log(`Saved ${word}`);
-
-//     // Call reloadList function
-//     reloadList();
-//   });
-// }
-
-function getFromLocal() {
-  /**
-   * This function does 2 things
-   * 1. Checks if wordList exists if True -> Returns it
-   * 2. Else -> returns True for not Does Not Exist
-   */
-  chrome.storage.local.get('wordList', (res) => {
-    console.log(res.wordList);
-  });
+      // Save list to localstorage
+      chrome.storage.local.set({ wordList: wordArray }, function () {
+        // On Save -> Call reloadList function
+        reloadList();
+      });
+    }); // .get end
+  } // if
 }
 
 function reloadList() {
@@ -112,48 +86,36 @@ function reloadList() {
   const listId = document.getElementById('list'); // Target list <ul>
 
   // A. Get list of words from localstorage
-  let wordList = getFromLocal(); // <- Previous List
   let htmlList = ``; // <- Will contain concatenated list.
 
-  console.log(wordList);
+  chrome.storage.local.get('wordList', ({ wordList }) => {
+    // wordList[0] === false => DNE
 
-  // wordList[0] === false => DNE
-  if (!wordList[0]) {
-    // B. Iterate through list of words, last-first.
+    if (typeof wordList !== 'undefined' && wordList.length > 0) {
+      // B. Iterate through list of words, last-first.
 
-    // Reverse loop array; Last entered would be top of the list.
-    for (let i = wordList.length - 1; i >= 0; i--) {
-      htmlList += `
-        <li id='${i}'>
-          <p>${wordList[i]}</p>
-          <button><img src="icons/close-icon.png" alt="close" /></button>
-        </li>
-      `;
+      // Reverse loop array; Last entered would be top of the list.
+      for (let i = wordList.length - 1; i >= 0; i--) {
+        htmlList += `
+      <li id='${i}'>
+        <p>${wordList[i]}</p>
+        <button><img src="icons/close-icon.png" alt="close" /></button>
+      </li>
+    `;
+      }
+
+      // C. Push concatenated list to DOM
+      listId.innerHTML = htmlList;
+    } else {
+      // C. Push concatenated list to DOM
+      listId.innerHTML = htmlList;
     }
-
-    console.log(wordList);
-    // C. Push concatenated list to DOM
-    listId.innerHTML = htmlList;
-  } else {
-    console.log(wordList);
-    // C. Push concatenated list to DOM
-    listId.innerHTML = htmlList;
-  }
+  });
 
   // TODO: implement message to background.js
 }
 
-function checkForList() {
-  chrome.storage.local.get('wordList', (result) => {
-    if (result.wordList === 'undefined') {
-      return false;
-    } else {
-      return true;
-    }
-  });
-}
-
-function deleteItem(word) {
+function deleteWord(word) {
   /**
    * This function does
    * 1. Gets previous list from localstorage. Check if exists.
@@ -161,4 +123,21 @@ function deleteItem(word) {
    * 3. Saves the new Array
    * 4. Calls reloadList();
    */
+
+  chrome.storage.local.get('wordList', ({ wordList }) => {
+    if (wordList.length > 1) {
+      // Loop through and remove
+      let newWordList = wordList.filter((el) => el !== word);
+      console.log(newWordList);
+
+      chrome.storage.local.set({ wordList: newWordList }, () => {
+        reloadList();
+      });
+    } else {
+      // since array contains only 1 item clear all of storage
+      chrome.storage.local.clear(() => {
+        reloadList();
+      });
+    }
+  });
 }
--- scripts/background.js
+++ scripts/background.js
@@ -3,10 +3,14 @@ console.log(`hello bawdad`);
 document.addEventListener('DOMContentLoaded', toListen, false);
 
 function toListen() {
+  storageWordCheck();
   // Listen for localstorage changes
   chrome.storage.onChanged.addListener(storageWordCheck);
 }
 
 function storageWordCheck() {
   console.log(`Local storage event listener test.`);
+  chrome.storage.local.get('wordList', ({ wordList }) => {
+    wordList.forEach((el) => console.log(el));
+  });
 }
