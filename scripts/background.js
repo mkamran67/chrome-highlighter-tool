b219dd1da1ccb0bbeab3847129fc015450d71025
Muhammad Kamran <mkamran1967@gmail.com>
2020-10-29 23:58:07 -0700

    Added a highlighting on "Refresh or Reload".

--- scripts/background.js
+++ scripts/background.js
@@ -1,37 +1,20 @@
 /**
- *  1. on storage change check
+ *
  *
  *
  *
  */
 
-// Colors
-/**
- *  RGB(255,195,0)
- *  RGB(237,221,83)
- *  RGB(173,212,92)
- *  RGB(87,199,133)
- *  RGB(0,186,173)
- *  RGB(42,123,155)
- *  RGB(61,61,107)
- *  RGB(81,24,73)
- *  RGB(144,12,63)
- *  RGB(199,0,57)
- *  RGB(255,87,51)
- *  RGB(255,141,26)
- *  RGB(255,195,0)
- */
+console.log(`hello Background`);
 
-console.log(`hello bawdad`);
+// document.addEventListener('DOMContentLoaded', toListen, false);
 
-document.addEventListener('DOMContentLoaded', toListen, false);
+// function toListen() {
+//   storageWordCheck();
+//   // Listen for localstorage changes
+//   chrome.storage.onChanged.addListener(storageWordCheck);
+// }
 
-function toListen() {
-  storageWordCheck();
-  // Listen for localstorage changes
-  chrome.storage.onChanged.addListener(storageWordCheck);
-}
-
-function storageWordCheck() {
-  console.log(`Local storage event listener test.`);
-}
+// function storageWordCheck() {
+//   console.log(`Local storage event listener test.`);
+// }
--- scripts/content.js
+++ scripts/content.js
@@ -9,18 +9,53 @@
  */
 
 // Check words and load em on current active tab
-
 let onload = () => {
-  console.log(`Content Script loaded`);
-
-  let word = 'the';
-
-  const regEx = new RegExp(`${word}`, 'gi');
+  console.log(`Highlighter loaded`);
 
   let listOfA = document.getElementsByTagName('p');
+  let listOfH3 = document.getElementsByTagName('h3');
+  let listOfEM = document.getElementsByTagName('em');
 
-  for (let i = 0; i < listOfA.length; i++) {
- // TODO: Highlight words within
+  // List of Words
+  chrome.storage.local.get('wordList', ({ wordList }) => {
+    if (typeof wordList !== 'undefined') {
+      wordList.forEach((word, index) => {
+        let color = index % 13;
+        const regEx = new RegExp(`${word}`, 'gi');
+
+        for (let i = 0; i < listOfA.length; i++) {
+          let index = listOfA[i].innerHTML.replaceAll(
+            regEx,
+            `<span class="highlight-color-${color}">  ${word} </span>`
+          );
+
+          listOfA[i].innerHTML = index;
+        }
+
+        for (let i = 0; i < listOfH3.length; i++) {
+          // console.log(listOfH3[i]);
+          let index = listOfH3[i].innerHTML.replaceAll(
+            regEx,
+            `<span class="highlight-color-${color}">${word}</span>`
+          );
+
+          listOfH3[i].innerHTML = index;
+        }
+        for (let i = 0; i < listOfEM.length; i++) {
+          // console.log(listOfEM[i]);
+          let index = listOfEM[i].innerHTML.replaceAll(
+            regEx,
+            `<span class="highlight-color-${color}">${word}</span>`
+          );
+
+          listOfEM[i].innerHTML = index;
+        }
+      });
+    }
+  });
 };
+
+// TODO: implement on word addition to rerun partial
+// function onEntry() {}
 
 onload();
