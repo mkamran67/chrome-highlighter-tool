4300f92c52854b16573a6ca4914b1e8b9bc6f96e
Muhammad Kamran <mkamran1967@gmail.com>
2020-11-03 22:10:52 -0800

    Webpack UP

--- scripts/content.js
+++ scripts/content.js
@@ -1,61 +1,19 @@
 /**
- * This script will run in the browser,
- * A few things this script will do.
- * 0. Start on all?Active tabs
- * 1. Check for local storage updates for words.
- * 2. On word addition/subtraction search and high light give list of words.
- * 3. Highlight word per color
- *
+ * This is the content script
  */
 
-// Check words and load em on current active tab
-let onload = () => {
-  console.log(`Highlighter loaded`);
+const Mark = require('mark.js');
 
-  let listOfA = document.getElementsByTagName('p');
-  let listOfH3 = document.getElementsByTagName('h3');
-  let listOfEM = document.getElementsByTagName('em');
+const onLoad = () => {
+  console.log(`Content Script`);
 
-  // List of Words
+  let context = document.getElementsByTagName('body');
+  let instance = new Mark(context);
+
   chrome.storage.local.get('wordList', ({ wordList }) => {
-    if (typeof wordList !== 'undefined') {
-      wordList.forEach((word, index) => {
-        let color = index % 13;
-        const regEx = new RegExp(`${word}`, 'gi');
-
-        for (let i = 0; i < listOfA.length; i++) {
-          let index = listOfA[i].innerHTML.replaceAll(
-            regEx,
-            `<span class="highlight-color-${color}">  ${word} </span>`
-          );
-
-          listOfA[i].innerHTML = index;
-        }
-
-        for (let i = 0; i < listOfH3.length; i++) {
-          // console.log(listOfH3[i]);
-          let index = listOfH3[i].innerHTML.replaceAll(
-            regEx,
-            `<span class="highlight-color-${color}">${word}</span>`
-          );
-
-          listOfH3[i].innerHTML = index;
-        }
-        for (let i = 0; i < listOfEM.length; i++) {
-          // console.log(listOfEM[i]);
-          let index = listOfEM[i].innerHTML.replaceAll(
-            regEx,
-            `<span class="highlight-color-${color}">${word}</span>`
-          );
-
-          listOfEM[i].innerHTML = index;
-        }
-      });
-    }
+    console.log(`Marking`);
+    instance.mark(wordList);
   });
 };
 
-// TODO: implement on word addition to rerun partial
-// function onEntry() {}
-
-onload();
+onLoad();
